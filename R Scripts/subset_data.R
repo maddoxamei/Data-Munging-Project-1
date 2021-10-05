f2575d33abc3b84eb82e35ac43da33ecb2df3088
Mei Maddox <86739836+meimaddox@users.noreply.github.com>
2021-10-05 10:44:30 -0400

    Subset data, collected Rmd file

---
+++ R Scripts/subset_data.R
@@ -0,0 +1,27 @@
+earnings.idx <- function(csb.data){
+  earn.mean <- grep('MN_EARN_WNE_P[10,8,6]', colnames(csb.data))
+  earn.med <- grep('MD_EARN_WNE_P[10,8,6]', colnames(csb.data))
+  earn.p <- grep('PCT[0-9]._EARN_WNE_P[10,8,6]', colnames(csb.data))
+  earn.sd <- grep('SD_EARN_WNE_P[10,8,6]', colnames(csb.data))
+  return( c(earn.mean, earn.med, earn.p, earn.sd) )
+}
+
+degree.idx <- function(csb.data){
+  return( grep("PCIP", colnames(csb.data)) )
+}
+
+tuition.idx <- function(csb.data){
+  return( c(which(colnames(csb.data)=="NPT4_PUB"):
+              which(colnames(csb.data)=="TUITIONFEE_PROG")) )
+}
+
+subset.data <- function(csb.data, size=1000, seed=NULL){
+  cols.idx <- c(earnings.idx(csb.data), 
+                degree.idx(csb.data), 
+                tuition.idx(csb.data))
+  
+  if( !is.null(seed) ) set.seed(seed)
+  rows.idx <- sample(nrow(csb.data), size)
+  
+  return( csb.data[rows.idx, cols.idx] )
+}
