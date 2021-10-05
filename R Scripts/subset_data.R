00e964fab8b6a802df17818c6da8b507e7b66a5b
Mei Maddox <86739836+meimaddox@users.noreply.github.com>
2021-10-05 16:24:37 -0400

    Earnings subset option changes
    Added option for all original earnings columns, Provide feedback by printing the dimension of the subsetted data

--- R Scripts/subset_data.R
+++ R Scripts/subset_data.R
@@ -1,9 +1,13 @@
-earnings.idx <- function(csb.data){
-  earn.mean <- grep('MN_EARN_WNE_P[10,8,6]', colnames(csb.data))
-  earn.med <- grep('MD_EARN_WNE_P[10,8,6]', colnames(csb.data))
-  earn.p <- grep('PCT[0-9]._EARN_WNE_P[10,8,6]', colnames(csb.data))
-  earn.sd <- grep('SD_EARN_WNE_P[10,8,6]', colnames(csb.data))
-  return( c(earn.mean, earn.med, earn.p, earn.sd) )
+earnings.idx <- function(csb.data, reduced=TRUE){
+  if( !reduced ) return( c(which(colnames(csb.2021)=="MN_EARN_WNE_P10"):
+                             which(colnames(csb.2021)=="GT_25K_P9")) )
+  else{
+    earn.mean <- grep('MN_EARN_WNE_P[10,8,6]', colnames(csb.data))
+    earn.med <- grep('MD_EARN_WNE_P[10,8,6]', colnames(csb.data))
+    earn.p <- grep('PCT[0-9]._EARN_WNE_P[10,8,6]', colnames(csb.data))
+    earn.sd <- grep('SD_EARN_WNE_P[10,8,6]', colnames(csb.data))
+    return( c(earn.mean, earn.med, earn.p, earn.sd) )
+  }
 }
 
 degree.idx <- function(csb.data){
@@ -15,8 +19,8 @@ tuition.idx <- function(csb.data){
               which(colnames(csb.data)=="TUITIONFEE_PROG")) )
 }
 
-subset.data <- function(csb.data, size=1000, seed=NULL){
-  cols.idx <- c(earnings.idx(csb.data), 
+subset.data <- function(csb.data, size=1000, seed=NULL, ...){
+  cols.idx <- c(earnings.idx(csb.data, ...), 
                 degree.idx(csb.data), 
                 tuition.idx(csb.data))
   
