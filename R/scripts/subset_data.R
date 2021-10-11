f1c5b754c1d9052f5459424bafe5cef957b247db
Mei Maddox <86739836+meimaddox@users.noreply.github.com>
2021-10-10 21:06:52 -0400

    rscorecard data dictionary

--- R/scripts/subset_data.R
+++ R/scripts/subset_data.R
@@ -1,27 +1,35 @@
-earnings.idx <- function(csb.data, reduced=TRUE){
-  if( !reduced ) return( c(which(colnames(csb.2021)=="MN_EARN_WNE_P10"):
-                             which(colnames(csb.2021)=="GT_25K_P9")) )
+earnings.idx <- function(csb.names, reduced=TRUE){
+  csb.names <- toupper(csb.names)
+  if( !reduced ) return( c(which(csb.names=="MN_EARN_WNE_P10"):
+                             which(csb.names=="GT_25K_P9")) )
   else{
-    earn.mean <- grep('MN_EARN_WNE_P[10,8,6]', colnames(csb.data))
-    earn.med <- grep('MD_EARN_WNE_P[10,8,6]', colnames(csb.data))
-    #earn.p <- grep('PCT[0-9]._EARN_WNE_P[10,8,6]', colnames(csb.data))
-    earn.sd <- grep('SD_EARN_WNE_P[10,8,6]', colnames(csb.data))
+    earn.mean <- grep('MN_EARN_WNE_P[10,8,6]', csb.names)
+    earn.med <- grep('MD_EARN_WNE_P[10,8,6]', csb.names)
+    #earn.p <- grep('PCT[0-9]._EARN_WNE_P[10,8,6]', csb.names)
+    earn.sd <- grep('SD_EARN_WNE_P[10,8,6]', csb.names)
     return( c(earn.mean, earn.med, earn.sd) )
   }
 }
 
-degree.idx <- function(csb.data){
-  return( grep("PCIP", colnames(csb.data)) )
+degree.idx <- function(csb.names){
+  csb.names <- toupper(csb.names)
+  return( grep("PCIP", csb.names) )
 }
 
-tuition.idx <- function(csb.data){
-  return( c(which(colnames(csb.data)=="COSTT4_A"):which(colnames(csb.data)=="TUITIONFEE_OUT")) )
+tuition.idx <- function(csb.names){
+  csb.names <- toupper(csb.names)
+  return( c(which(csb.names=="COSTT4_A"):which(csb.names=="TUITIONFEE_OUT")) )
+}
+
+sample.data <- function(csb.data, size=1000, seed=NULL){
+  if( !is.null(seed) ) set.seed(seed)
+  return( csb.data[sample(nrow(csb.data), size),] )
 }
 
 subset.data <- function(csb.data, size=1000, seed=NULL, ...){
-  cols.idx <- c(earnings.idx(csb.data, ...), 
-                degree.idx(csb.data), 
-                tuition.idx(csb.data))
+  cols.idx <- c(earnings.idx(colnames(csb.data), ...), 
+                degree.idx(colnames(csb.data)), 
+                tuition.idx(colnames(csb.data)))
   
   if( !is.null(seed) ) set.seed(seed)
   rows.idx <- sample(nrow(csb.data), size)
